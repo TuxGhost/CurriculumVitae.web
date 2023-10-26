8d5cfa5eac049a73a471db221ba8bb2f7b87345d
Peter Kuda <PETER.KUDA@vdabcampus.be>
2023-10-26 19:38:49 +0200

    toevoegen  van UseForwardedHeaders

--- peterkuda.be/Program.cs
+++ peterkuda.be/Program.cs
@@ -1,9 +1,14 @@
+using Microsoft.AspNetCore.HttpOverrides;
 using CurriculumVitae.Services;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+builder.Services.Configure<ForwardedHeadersOptions>(options =>
+    options.ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
+); // For use behind a reverse proxy server
 builder.Services.AddTransient<ICvService, InternalCvService>();
 
 var app = builder.Build();
@@ -13,7 +18,8 @@ if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    //app.UseHsts(); // Enforce SSL
+    app.UseForwardedHeaders(); // for use behind a reverse proxy server
+    app.UseHsts(); // Enforce SSL
 }
 
 //app.UseHttpsRedirection();
