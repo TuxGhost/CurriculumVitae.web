5b9fa8db12c5fd3f9a4a44ab2f2308fb544b7a5e
Peter Kuda <PETER.KUDA@vdabcampus.be>
2023-12-14 12:23:36 +0100

    added SQLite service for just retrieval of data and layout adjustments

--- peterkuda.be/Services/SQLiteCvService.cs
+++ peterkuda.be/Services/SQLiteCvService.cs
@@ -1,12 +1,23 @@
-﻿using CurriculumVitae.Model;
+﻿using CurriculumVitae.Data;
+using CurriculumVitae.Model;
+using Microsoft.EntityFrameworkCore.Diagnostics;
 
 namespace CurriculumVitae.Services;
 
 public class SQLiteCvService : ICvService
 {
     private CvModel cvModels = null!;
-    public CvModel GetCv()
+    private CurriculumVitaeDbContext dbContext = null!;
+    public SQLiteCvService(CurriculumVitaeDbContext dbContext)
     {
-        return cvModels;
+        this.dbContext = dbContext;
     }
+    public CvModel GetCv()
+    {        
+        if(cvModels == null)
+        {
+            cvModels = new CvModel();
+        }
+        return cvModels;
+    }    
 }
