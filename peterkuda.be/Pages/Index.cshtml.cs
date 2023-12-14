b43f3a96cd7a12e725e04959f3d27e830da095eb
Peter Kuda <PETER.KUDA@vdabcampus.be>
2023-12-14 22:13:07 +0100

    Toevoegen van taal en computervaardigheden

--- peterkuda.be/Pages/Index.cshtml.cs
+++ peterkuda.be/Pages/Index.cshtml.cs
@@ -10,11 +10,15 @@ public class IndexModel : PageModel
 {
     private readonly ILogger<IndexModel> _logger;
     private readonly ICvService cvService;
-    [BindProperty]        
+    [BindProperty]
     public CvModel cv { get; private set; } = null!;
+    [BindProperty]
+    public TaalModel Taal { get; private set; } = null!;
+    [BindProperty]
+    public ComputerVaardigheid ComputerVaardigheid { get; private set; } = null!;
     public bool Editable { get; set; } = false;
 
-    public IndexModel(ILogger<IndexModel> logger,ICvService cvService)  
+    public IndexModel(ILogger<IndexModel> logger, ICvService cvService)
     {
         _logger = logger;
         this.cvService = cvService;
@@ -27,27 +31,44 @@ public class IndexModel : PageModel
     }
     public void OnPost()
     {
-        
+
     }
-    public void OnPostEdit(string editable,string profiel)
+    public void OnPostEdit(string editable, string profiel)
     {
         //cv.Profiel = Request.Form["profiel"];
-        
+
         if (Editable)
-        {                
+        {
             Editable = false;
         }
         else
         {
             Editable = true;
         }
-        cv.Editable=true;        
-    }               
+        cv.Editable = true;
+    }
     public void OnPostProfiel(CvModel cv)
     {
         if (cv != null)
         {
             cvService.SetProfiel(cv.Profiel!);
-        }        
+        }
+    }
+    public void OnPostAddlanguage(TaalModel taal)
+    {
+        cvService.AddLanguage(new CurriculumVitae.Data.Entities.TaalModel
+        {
+            Taal = taal.Taal,
+            Niveau = taal.Niveau,   
+        });
+    }
+    public void OnPostAddcomputerskill(ComputerVaardigheid computervaardigheid)
+    {
+        cvService.AddComputerServices(new CurriculumVitae.Data.Entities.ComputerVaardigheid
+        {
+            Category =" ",
+            Niveau = computervaardigheid.Niveau,
+            Omschrijving = computervaardigheid.Omschrijving
+        });
     }
 }
