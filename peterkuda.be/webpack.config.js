ba560be12bfc1e7442f517f208c573c143345ddf
Peter Kuda <petar@outlook.be>
2024-01-02 12:27:10 +0100

    install an empty bundle.js as example for webpack

---
+++ peterkuda.be/webpack.config.js
@@ -0,0 +1,10 @@
+﻿const path = require('path');
+
+module.exports = {
+    entry: './src/index.js', // Replace with your entry file
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'wwwroot','dist'), // Replace with your output directory
+        
+    },
+};
