d916dda636a3abc1cd19785ffea8f50bcf96207d
Peter Kuda <petar@outlook.be>
2024-01-14 11:49:15 +0100

    preparation webpack

---
+++ peterkuda.be/webpack.config.js
@@ -0,0 +1,9 @@
+﻿const path = require('path');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'bootstrap.js',
+        path: path.resolve(__dirname,'wwwroot/dist'),
+    },
+};
