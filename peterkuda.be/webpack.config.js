1d741160fab8d9880bb42c9ce8692be73d41bab4
Peter Kuda <petar@outlook.be>
2024-01-14 12:22:04 +0100

    Include bootstrap 5.1.0

--- peterkuda.be/webpack.config.js
+++ peterkuda.be/webpack.config.js
@@ -1,9 +1,22 @@
 ﻿const path = require('path');
 
 module.exports = {
-    entry: './src/index.js',
+    entry: './src/js/index.js',
     output: {
         filename: 'bootstrap.js',
         path: path.resolve(__dirname,'wwwroot/dist'),
     },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ['style-loader', {
+                    loader: 'css-loader',
+                    options: {
+                        sourceMap: true,
+                    },
+                }]
+            },
+        ]
+    }
 };
